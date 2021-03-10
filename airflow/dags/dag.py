00126f0cf2d45ad8bec4b661cfc4b434effd975f
offor20 <ernestoffor@gmail.com>
2021-03-10 10:29:09 +0100

    implement StageToRedshiftOperator

--- airflow/dags/dag.py
+++ airflow/dags/dag.py
@@ -7,21 +7,21 @@ from airflow.operators import (StageToRedshiftOperator, LoadFactOperator,
 from airflow.operators.postgres_operator import PostgresOperator
 from helpers import SqlQueries
 
-AWS_KEY = os.environ.get('AWS_KEY')
-AWS_SECRET = os.environ.get('AWS_SECRET')
+#AWS_KEY = os.environ.get('AWS_KEY')
+#AWS_SECRET = os.environ.get('AWS_SECRET')
 
 default_args = {
     'owner': 'udacity',
     'start_date': datetime(2019, 1, 12),
     'depends_on_past': False,
     'email': ['ernest@gmail.com.com'],
-    'email_on_failure': True,
+    'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
     'retry_delay': timedelta(minutes=5)
 }
 
-dag = DAG('udac_example_dag',
+dag = DAG('project_dag',
           default_args=default_args,
           description='Load and transform data in Redshift with Airflow',
           schedule_interval='@monthly'
@@ -38,12 +38,23 @@ create_tables_task = PostgresOperator(
 
 stage_events_to_redshift = StageToRedshiftOperator(
     task_id='Stage_events',
-    dag=dag
+    dag=dag,
+    redshift_conn_id="redshift",
+    aws_credentials_id ="aws_credentials",
+    staging_table = "staging_events",
+    s3_bucket ="udacity-dend",
+    s3_key = "log_data/2018/11/",
+    json_path = 'log_json_path.json'
 )
 
 stage_songs_to_redshift = StageToRedshiftOperator(
     task_id='Stage_songs',
-    dag=dag
+    dag=dag,
+    redshift_conn_id="redshift",
+    aws_credentials_id ="aws_credentials",
+    staging_table = "staging_songs",
+    s3_bucket='udacity-dend',
+    s3_key='song_data/*/*/*/'
 )
 
 load_songplays_table = LoadFactOperator(
@@ -98,9 +109,9 @@ run_quality_checks = DataQualityOperator(
 
 end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
 
-#start_operator >> create_tables_task
-start_operator >> stage_events_to_redshift
-start_operator>> stage_songs_to_redshift
+start_operator >> create_tables_task
+create_tables_task >> stage_events_to_redshift
+create_tables_task >> stage_songs_to_redshift
 stage_songs_to_redshift >> load_songplays_table
 stage_events_to_redshift >> load_songplays_table
 
--- airflow/plugins/operators/stage_redshift.py
+++ airflow/plugins/operators/stage_redshift.py
@@ -3,22 +3,69 @@ from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
 class StageToRedshiftOperator(BaseOperator):
+    copy_table_sql = """
+                 COPY {} 
+                 FROM '{}'
+                 REGION '{}'
+                 aws_access_key_id  '{}'
+                 aws_secret_access_key '{}'
+                 FORMAT AS JSON '{}'
+                      """
+    
+    delete_table_sql = """
+                        DELETE FROM {} 
+                        """
+
     ui_color = '#358140'
+
+    template_fields = ("s3_key",)
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # redshift_conn_id=your-connection-name
+                 redshift_conn_id="",
+                 aws_credentials_id ="",
+                 staging_table = "",
+                 s3_bucket ="",
+                 s3_region = "us-west-2",
+                 s3_key = "",
+                 json_path="auto",
+
+
+                 
                  *args, **kwargs):
 
         super(StageToRedshiftOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
-
+        self.redshift_conn_id= redshift_conn_id
+        self.aws_credentials_id =aws_credentials_id
+        self.staging_table =staging_table
+        self.s3_bucket =s3_bucket
+        self.s3_region = s3_region
+        self.s3_key = s3_key
+        self.json_path=json_path
+        
     def execute(self, context):
-        self.log.info('StageToRedshiftOperator not implemented yet')
+        self.log.info('Start implementation of StageToRedshiftOperator')
+        aws_hook = AwsHook(self.aws_credentials_id)
+        credentials = aws_hook.get_credentials()
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id) 
+        
+        self.log.info("Preparing destination Redshift table")
+        redshift.run( StageToRedshiftOperator.delete_table_sql.format(self.staging_table))
+        
+        self.log.info("Getting data from S3 to Redshift")
+        rendered_s3_key = self.s3_key.format(**context)
+        s3_path = "s3://{}/{}".format(self.s3_bucket, rendered_s3_key)
+        execution_sql = StageToRedshiftOperator.copy_table_sql.format(
+            self.staging_table,
+            s3_path,
+            self.s3_region,
+            credentials.access_key,
+            credentials.secret_key,
+            self.json_path
+            
+          )
+        redshift.run(execution_sql)
+        
 
 
 
